e1079e86973320a661e3b41b1b0802151c8b6a54
Sendach <Sendachwt@gmail.com>
2023-05-07 03:41:06 +0200

    Added ColorSelector as a common component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as CanvasManager } from './CanvasManager/CanvasManager';
 export { default as Drawing } from './Drawing/Drawing';
 export { default as Toolbar } from './Toolbar/Toolbar';
 export { default as SettingsBar } from './SettingsBar/SettingsBar';
+export { default as ColorSelector } from './ColorSelector/ColorSelector';
